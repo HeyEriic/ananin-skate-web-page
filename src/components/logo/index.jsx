83b19e7a34ae721068fc73a16c2d554229df399f
Eric Nascimento <eriicdiiego@hotmail.com>
2024-04-30 13:40:27 -0300

    feat: add logo component

---
+++ src/components/logo/index.jsx
@@ -0,0 +1,12 @@
+import "./style.css";
+
+export default function Logo() {
+  return (
+    <div id="logo-container">
+      <img id="logo"
+        src="/images/ananin_skate_logo.jpeg"
+        alt="Logo Ananin Skate Social"
+      />
+    </div>
+  );
+}
