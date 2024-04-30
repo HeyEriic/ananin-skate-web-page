3f0069083613136909ff8136c5db56ccd8482fba
Eric Nascimento <eriicdiiego@hotmail.com>
2024-04-30 13:41:47 -0300

    feat: add navbar component

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,39 @@
+import "./style.css";
+
+export default function Navbar() {
+  return (
+    <div id="nav-container">
+      <ul id="nav-list">
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Home
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Sobre
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Eventos
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Contato
+          </a>
+        </li>
+      </ul>
+      <div className="dropdown" id="dropdown">
+        <button className="dropbtn">Menu</button>
+        <div className="dropdown-content">
+          <a href="#">Home</a>
+          <a href="#">Sobre</a>
+          <a href="#">Eventos</a>
+          <a href="#">Contato</a>
+        </div>
+      </div>
+    </div>
+  );
+}
