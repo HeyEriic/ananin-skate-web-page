40dbb75758dd28f6c83e7f3c5a5a152eff257e21
Eric Nascimento <eriicdiiego@hotmail.com>
2024-05-02 15:42:39 -0300

    (refactor | style): modify Navbar component

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
-
 import "./style.css";
 
 export default function Navbar() {
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
+
   return (
     <div id="nav-container">
       <hr id="line-nav" />
@@ -29,10 +35,14 @@ export default function Navbar() {
         </li>
       </ul>
       <div className="dropdown" id="dropdown">
-        <button className="dropbtn">
-          <RxHamburgerMenu className="hamburguerBtn"/> Menu
+        <button className="dropbtn" onClick={toggleDropdown}>
+          <RxHamburgerMenu className="hamburguerBtn" /> Menu
         </button>
-        <div className="dropdown-content">
+        <div
+          className={
+            dropdownVisible ? "dropdown-content show" : "dropdown-content"
+          }
+        >
           <a href="#">Home</a>
           <a href="#">Sobre</a>
           <a href="#">Eventos</a>
